1dc6685f78f3a7df9d037d19d4d106cfaf653b25
Tetiana Pyatachuk <pyatachuk26@gmail.com>
2026-02-01 15:12:36 +0200

    person1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
 // export const Person = ({ person }) => ();
+export const Person = ({ name, age, sex, isMarried = false, partnerName }) => (
+  <section className="Person">
+
+     <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+    <p className="Person__age">{age && `I am ${age}`}</p>
+
+    <p className="Person__partner">
+    {(() => {
+        if (!isMarried) return 'I am not married';
+        return sex === 'm'
+          ? `${partnerName} is my wife`
+          : `${partnerName} is my husband`;
+      })()}
+    </p>
+  </section>
+);
